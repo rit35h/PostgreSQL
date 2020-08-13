6071d92ffeb562a3d872dc6edee06f73bdcbb618
Ritesh <Ritesh@Ritesh-Lappy>
2020-08-13 17:05:56 +0530

    final update at 05:05PM

--- Springboot-postgrsql-hiber-crud-ex/src/main/java/com/example/demo/controller/EmployeeController.java
+++ Springboot-postgrsql-hiber-crud-ex/src/main/java/com/example/demo/controller/EmployeeController.java
@@ -1,4 +1,5 @@
 package com.example.demo.controller;
+import javax.validation.Valid;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -21,6 +22,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.repository.EmployeeRepository;
+
 
 
 
